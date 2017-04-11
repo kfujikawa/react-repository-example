b42009fb61281852c8d2a1dcf546700118d0898b
Kinuyo Fujikawa <kinuyofujikawa@gmail.com>
2017-04-11 14:10:47 -0700

    updated examples

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
-require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 
+import store from './store';
+import RepositoryList from './components/repository-list';
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(
+        <Provider store={store}>
+            <RepositoryList />
+        </Provider>,
+        document.getElementById('app')
+    )
+);
+
+// import * as actions from './actions/index';
+// import * as reducers from './reducers/index';
+// import store from './store';
+
+// Allows for actions to be run through reducer
+// store.dispatch(actions.addRepository('joe'));
+
+//getState inspect current state held in store Logs [{ name: 'joe', rating: null}]
+// console.log(store.getState()); 
---
+++ js/reducers/index.js
@@ -0,0 +1,72 @@
+import * as actions from '../actions/index';
+
+// Initial state
+const initialRepositoryState = [];
+
+export const repositoryReducer = (state=initialRepositoryState, action) => {
+    if (action.type === actions.ADD_REPOSITORY) {
+        return [...state, {
+            name: action.repository,
+            rating: null
+        }];
+    }
+    else if (action.type === actions.RATE_REPOSITORY) {
+        // Find the index of the matching repository
+        const index = state.findIndex(repository =>
+            repository.name === action.repository
+        );
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        const before = state.slice(0, index);
+        const after = state.slice(index + 1);
+        const newRepository = Object.assign({}, state[index], {rating: action.rating});
+        return [...before, newRepository, ...after];
+    }
+
+    else if (action.type === actions.FETCH_DESCRIPTION_SUCCESS) {
+        // Find the index of the matching repository
+        const index = state.findIndex(repository =>
+            repository.name === action.repository
+        );
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        const before = state.slice(0, index);
+        const after = state.slice(index + 1);
+        const newRepository = Object.assign({}, state[index], {
+            description: action.description
+        });
+        return [...before, newRepository, ...after];
+    }
+
+    else if (action.type === actions.FETCH_DESCRIPTION_ERROR) {
+        // Find the index of the matching repository
+        const index = state.findIndex(repository =>
+            repository.name === action.repository
+        );
+
+        if (index === -1) {
+            throw new Error('Could not find repository');
+        }
+
+        const before = state.slice(0, index);
+        const after = state.slice(index + 1);
+        const newRepository = Object.assign({}, state[index], {
+            description: 'N/A'
+        });
+        return [...before, newRepository, ...after];
+    }
+
+    return state;
+};
+
+// const initialRepositoryState = [];
+
+// export const repositoryReducer = (state=initialRepositoryState, action) => {
+//     return state;
+// };
